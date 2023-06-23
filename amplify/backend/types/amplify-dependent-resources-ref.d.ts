aceddd7f6664c9a2160e0c8a4e2c6e4f8fbc2139
yuhuimin1996 <huiminyu51@gmail.com>
2023-06-22 18:01:03 -0700

    add api + Lambda + auth + graphql

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,29 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "inspirationalquotes": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "inspirationalquotes472e2244": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "inspoquoteLambda": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,81 @@
+import React,{ useState } from 'react'
+
 import Head from 'next/head'
 import Image from 'next/image'
-
 import styles from '@/styles/Home.module.css'
-import { GradientBackgroundCon } from '@/components/QouteGenerator/QouteGeneratorElements'
+
+
+//components
+import { BackgroundImage1, BackgroundImage2, FooterCon, FooterLink, GenerateQuoteButton, GenerateQuoteButtonText, GradientBackgroundCon, QuoteGeneratorCon, QuoteGeneratorInnerCon, QuoteGeneratorSubTitle, QuoteGeneratorTitle, RedSpan } from '@/components/QuoteGenerator/QuoteGeneratorElements'
+
+//assets
+import Clouds1 from '../assets/cloud-and-thunder.png'
+import Clouds2 from '../assets/cloudy-weather.png'
 
 
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Inspirational Qoute Generator</title>
-        <meta name="description" content="A fun project to generate qoutes" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      {/* Background */}
-      <GradientBackgroundCon>
-        
-      </GradientBackgroundCon>
-    </>
-  )
+
+  const [numberOfQuotes, setNumberOfQuotes] = useState<number | null>(0);
+
+    return (
+      <>
+        <Head>
+          <title>Inspirational Quote Generator</title>
+          <meta name="description" content="A fun project to generate quotes" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        {/* Background */}
+        <GradientBackgroundCon>
+
+          {/* Quote Generator Modal Pop-Up */}
+          {/* <QuoteGeneratorModal
+          /> */}
+
+          {/* Quote Generator */}
+          <QuoteGeneratorCon>
+            <QuoteGeneratorInnerCon>
+              <QuoteGeneratorTitle>
+                Daily Inspiration Generator
+              </QuoteGeneratorTitle>
+
+              <QuoteGeneratorSubTitle>
+                Looking for a splash of inspiration? Generate a quote card with a random inspirational quote provided by <FooterLink href="https://zenquotes.io/" target="_blank" rel="noopener noreferrer">ZenQuotes API</FooterLink>.
+              </QuoteGeneratorSubTitle>
+
+              <GenerateQuoteButton onClick={null}>
+                <GenerateQuoteButtonText>
+                  Make a Quote
+                </GenerateQuoteButtonText>
+              </GenerateQuoteButton>
+            </QuoteGeneratorInnerCon>
+          </QuoteGeneratorCon>
+  
+          {/* Background Images */}
+          <BackgroundImage1 
+            src={Clouds1}
+            height="300"
+            alt="cloudybackground1"
+          />
+  
+          <BackgroundImage2
+            src={Clouds2}
+            height="300"
+            alt="cloudybackground1"
+          />
+  
+          {/* Footer */}
+          <FooterCon>
+            <>
+              Quotes Generated: {numberOfQuotes}
+              <br />
+              Developed with <RedSpan>♥</RedSpan> by <FooterLink href="https://www.linkedin.com/in/huimin-yu/" target="_blank" rel="noopener noreferrer"> @HuiminYu </FooterLink>
+            </>
+          </FooterCon>
+  
+        </GradientBackgroundCon>
+      </>
+    )
+  
 }
